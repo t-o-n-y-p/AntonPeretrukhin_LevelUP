e423b7abb7350199f4958516c161bf3d3c1c46c7
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2021-06-19 14:26:00 +0300

    added exceptions and examples (task 1)

--- src/main/java/ru/levelp/at/hw3/aircrafts/Aircraft.java
+++ src/main/java/ru/levelp/at/hw3/aircrafts/Aircraft.java
@@ -1,15 +1,25 @@
 package ru.levelp.at.hw3.aircrafts;
 
+import ru.levelp.at.hw4.task1.exceptions.AircraftLoadException;
+import ru.levelp.at.hw4.task1.exceptions.AircraftParamsException;
+import ru.levelp.at.hw4.task1.exceptions.AircraftStateException;
+
 public abstract class Aircraft {
 
     private final Double capacity;
     private final Integer maxSpeed;
     private final Integer maxRange;
+    private Double load;
+    protected boolean flightInProgress;
 
     public Aircraft(Double capacity, Integer maxSpeed, Integer maxRange) {
+        if (capacity <= 0 || maxSpeed <= 0 || maxRange <= 0) {
+            throw new AircraftParamsException("Aircraft params must be positive.");
+        }
         this.capacity = capacity;
         this.maxSpeed = maxSpeed;
         this.maxRange = maxRange;
+        this.load = 0.0;
     }
 
     public Double getCapacity() {
@@ -23,4 +33,44 @@ public abstract class Aircraft {
     public Integer getMaxRange() {
         return maxRange;
     }
+
+    public Double getLoad() {
+        return load;
+    }
+
+    public boolean isFlightInProgress() {
+        return flightInProgress;
+    }
+
+    public void startFlight() {
+        flightInProgress = true;
+    }
+
+    public void endFlight() {
+        flightInProgress = false;
+    }
+
+    public void load(Double weight) {
+        if (flightInProgress) {
+            throw new AircraftStateException("Aircraft is in flight; can't load.");
+        }
+        if (capacity - load < weight) {
+            throw new AircraftLoadException("No room for the load with the given weight.");
+        }
+
+        load += capacity;
+    }
+
+    public void unload(Double weight) {
+        if (flightInProgress) {
+            throw new AircraftStateException("Aircraft is in flight; can't unload.");
+        }
+        if (load < weight) {
+            throw new AircraftLoadException("Current load is less than the given weight.");
+        }
+
+        load -= capacity;
+    }
+
+
 }
---
+++ src/main/java/ru/levelp/at/hw4/task1/exceptions/AircraftStateException.java
@@ -0,0 +1,8 @@
+package ru.levelp.at.hw4.task1.exceptions;
+
+public class AircraftStateException extends RuntimeException {
+
+    public AircraftStateException(String message) {
+        super(message);
+    }
+}
